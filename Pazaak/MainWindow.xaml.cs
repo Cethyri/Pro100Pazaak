a68b2c31571462c7489000b1906bea5a070c8b2f
Unknown <CRobinson@student.neumont.edu>
2017-11-15 18:22:10 -0700

    Format HandControl
    added a listbox to contain all cards in hand

--- Pazaak/MainWindow.xaml.cs
+++ Pazaak/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pazaak.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace Pazaak
     {
         public MainWindow()
         {
-            InitializeComponent();
+
         }
     }
 }
