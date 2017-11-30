8740e76b5a801a3b8674da6a4e22624ef7ee043d
Zac Snowbank <ZSnowbank@student.neumont.edu>
2017-11-29 17:41:47 -0700

    Added begin turn and end turn functions to Player. Added delegate to hold the next player's BeginTurn() to be called in EndTurn()

--- Pazaak/Player.cs
+++ Pazaak/Player.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace Pazaak
 {
+    public delegate void NextPlayerBeginTurn();
+
     class Player : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
@@ -20,13 +22,46 @@ namespace Pazaak
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(field));
         }
 
+        /// <summary>
+        /// Starts turn by adding a card from the main deck to the player's board.
+        /// also sets IsActive to true
+        /// </summary>
+        public void BeginTurn()
+        {
+            IsActive = true;
+
+            Cards.ICard newCard = /*random card from main deck*/ new Cards.ValueCard(0);
+            //remove newCard from main deck
+            Board.AddCard(newCard);
+        }
+
+        /// <summary>
+        /// Ends the turn by turning IsActive to false 
+        /// and calling the BeginTurn method for the next player
+        /// </summary>
+        public void EndTurn()
+        {
+            IsActive = false;
+
+            nextPlayerBeginTurn();
+        }
+
+        /// <summary>
+        /// Add's the nextPlayer's begin turn method to the delegate called in EndTurn()
+        /// </summary>
+        /// <param name="nextPlayer"> the next player in turn order </param>
+        public void RegisterNextPlayer(Player nextPlayer)
+        {
+            nextPlayerBeginTurn += nextPlayer.BeginTurn;
+        }
+
         private bool isActive;
         private int wins;
         private string name;
         private Deck sideDeck;
         private Hand hand;
         private Board board;
-
+        private NextPlayerBeginTurn nextPlayerBeginTurn;
 
         public bool IsActive
         {
@@ -82,6 +117,7 @@ namespace Pazaak
                 FieldChanged("Board");
             }
         }
+
         public Player()
         {
             sideDeck = new Deck();
