26a0a3ed25f9b3f256a0495efb4039bd2df6c8b0
Zac Snowbank <ZSnowbank@student.neumont.edu>
2017-11-30 18:04:05 -0700

    Players now skip their turn if they stood a previous turn

--- Pazaak/Player.cs
+++ Pazaak/Player.cs
@@ -24,13 +24,21 @@ namespace Pazaak
 
         /// <summary>
         /// Starts turn by adding a card from the main deck to the player's board.
+        /// If the player has stood, instead they skip they're turn
         /// also sets IsActive to true
         /// </summary>
         public void BeginTurn()
         {
-            IsActive = true;
-            
-            Board.AddCard(MainDeck.DrawNextCard());
+            if (HasStood)
+            {
+                EndTurn();
+            }
+            else
+            {
+                IsActive = true;
+
+                Board.AddCard(MainDeck.DrawNextCard());
+            }
         }
 
         /// <summary>
@@ -56,6 +64,7 @@ namespace Pazaak
         }
 
         private bool isActive;
+        private bool hasStood;
         private int wins;
         private string name;
         private Deck sideDeck;
@@ -71,6 +80,16 @@ namespace Pazaak
             {
                 isActive = value;
                 FieldChanged("IsActive");
+            }
+        }
+
+        public bool HasStood
+        {
+            get => hasStood;
+            set
+            {
+                hasStood = value;
+                FieldChanged("HasStood");
             }
         }
         public int Wins
