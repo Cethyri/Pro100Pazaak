2a9d245502528ac2b1e8a60a8a2781372f31bf4c
Unknown <CRobinson@student.neumont.edu>
2017-11-30 17:33:03 -0700

    Disable Player Control
    Player Control is Disabled when Player.IsActive is false

---
+++ Pazaak/Converters/DataContextToBrushConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Pazaak.Converters
+{
+    public class BooleanIsActiveToBrushForegroundConverter : IValueConverter
+    {
+        public static SolidColorBrush trueColor = new SolidColorBrush(Color.FromArgb(255, 127, 127, 127));
+        public static SolidColorBrush falseColor = new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (typeof(bool) == value.GetType())
+            {
+                return (bool)value ? trueColor : falseColor;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (typeof(SolidColorBrush) == value.GetType())
+            {
+                if (value.Equals(trueColor))
+                {
+                    return true;
+                }
+                else if (value.Equals(falseColor))
+                {
+                    return false;
+                }
+            }
+
+            return null;
+        }
+    }
+}
