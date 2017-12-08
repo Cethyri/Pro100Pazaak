6d89175e6be5c4cc63538d57c39f1e7acffb7b20
Unknown <CRobinson@student.neumont.edu>
2017-12-08 16:10:16 -0700

    Add functionality to card selector control

--- Pazaak/UserControls/CardSelectorControl.xaml.cs
+++ Pazaak/UserControls/CardSelectorControl.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Pazaak.Cards;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -17,21 +19,61 @@ namespace Pazaak.UserControls
     /// <summary>
     /// Interaction logic for CardSelectorControl.xaml
     /// </summary>
-    public partial class CardSelectorControl : UserControl
+    public partial class CardSelectorControl : INotifyPropertyChanged
     {
-        public CardSelectorControl()
+        /// <summary>
+        /// Notifies all bindings that a property has changed
+        /// </summary>
+        /// <param name="field"> Name of property changed </param>
+        protected void FieldChanged(string field = null)
         {
-            InitializeComponent();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(field));
+        }
+
+        private void CardSelectorControl_Closing(object sender, CancelEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
         private void CardControlSelection_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            CardControl card = sender as CardControl;
 
+            Deck sideDeck = DataContext as Deck;
+
+            sideDeck.AddCard((card.DataContext as ICard).Copy());
         }
 
         private void CardControlSideDeck_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            CardControl card = sender as CardControl;
 
+            Deck sideDeck = DataContext as Deck;
+
+            sideDeck.Cards.Remove(card.DataContext as ICard);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICard[] selectionCards;
+
+        public ICard[] SelectionCards
+        {
+            get
+            {
+                return selectionCards;
+            }
+            set
+            {
+                selectionCards = value;
+                FieldChanged("SelectionCards");
+            }
+        }
+
+        public CardSelectorControl()
+        {
+            InitializeComponent();
+
+            Closing += CardSelectorControl_Closing;
         }
     }
 }
